e35ac8150c7b70b61afb88c887a4cad0d221e924
Black-Soldier <119063254+Bllack-Soldier@users.noreply.github.com>
2024-02-20 15:39:47 +0330

    Add files via upload

---
+++ Check-Bot.psm1
@@ -0,0 +1,60 @@
+﻿function Check-Bot {
+[CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='Medium')]
+    
+#this static cmdlet just call by another process to check specified processes and call result back 
+
+
+$bot1="process name 1"
+$bott2="process name 2"
+$bot3="process name 3"
+$bot4="process name 4"
+$bot5="process name 5"
+$bot6="process name 6"
+$bott7="process name 7"
+$bott8="process name 8"
+$bott9="process name 9"
+$bot10="process name 10"
+
+
+    $list1=$bott2,$bott7,$bott8,$bott9
+    $list2=$bot1,$bot3,$bot4,$bot5,$bot6,$bot10
+    $location1="D:\files"
+    $location2="D:\files"
+
+
+foreach($bot in $list2){
+        
+            $wa= Get-Process -Name $bot -ErrorAction SilentlyContinue
+
+    if($wa -ne $null){Write-Host "$bot is Connected"}else {
+        Write-Verbose "Reconnecting..."
+
+        
+        cd $location2
+        
+        Start-Process .\$bot
+
+        Write-Verbose "$bot is Connected" }
+}
+
+
+
+
+foreach($bott in $list1){
+
+            $wa= Get-Process -name $bott -ErrorAction SilentlyContinue
+
+    if($wa -ne $null){Write-Host "$bott is Connected"}else {
+        Write-Verbose "Reconnecting..."
+
+        
+        cd $location1
+        
+        Start-Process .\$bott
+
+        Write-Verbose "$bott is Connected" }
+
+
+
+}
+}
---
+++ caller.ps1
@@ -0,0 +1,10 @@
+﻿$pass="your-password"
+$username="your-username"
+$sec_string_pass=ConvertTo-SecureString $pass -AsPlainText -Force
+$credob=new-object System.Management.Automation.PSCredential ($username,$sec_string_pass)
+$s=New-PSSession -ComputerName 127.0.0.1 -Credential $credob
+
+Invoke-Command -Session $s -ScriptBlock{Check-Bot
+Get-PSSession | Remove-PSSession}
+Disconnect-PSSession -session $s
+
